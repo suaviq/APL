14b44d759ce31a62cc24869727b78e4f17a9b4e6
Maciej Krzyżowski <macikrz683@student.polsl.pl>
2023-01-19 11:15:37 +0100

    fixes + add/sub matrices

--- ASM_dll/nn.asm
+++ ASM_dll/nn.asm
@@ -47,8 +47,8 @@ vector_wise_multiply endp
     ;; === OPERATIONS WITH SCALARS ===
 add_scalar_to_vector proc
     vmovupd ymm1, [rcx]             ; ymm1 <- array A at addr rcx
-    vmovapd xmm2, [rdx]			; xmm2 <- scalar
-    vbroadcastsd ymm2, xmm2		; ymm2 <- low part of xmm2 * 4
+    vmovapd xmm2, [rdx]             ; xmm2 <- scalar
+    vbroadcastsd ymm2, xmm2         ; ymm2 <- low part of xmm2 * 4
     vaddpd ymm1, ymm1, ymm2         ; ymm1 = ymm1 + ymm2
     vmovupd [rcx], ymm1             ; A <- ymm1 (A + scalar)
     ret
@@ -56,18 +56,18 @@ add_scalar_to_vector endp
 
 subtract_scalar_from_vector proc
     vmovupd ymm1, [rcx]             ; ymm1 <- array A at addr rcx
-    vmovapd xmm2, [rdx]			; xmm2 <- scalar
-    vbroadcastsd ymm2, xmm2		; ymm2 <- low part of xmm2 * 4
-    vsubpd ymm1, ymm1, ymm2		; ymm1 = ymm1 - ymm2
-    vmovupd [rcx], ymm1			; A <- ymm1 (A - scalar)
+    vmovapd xmm2, [rdx]             ; xmm2 <- scalar
+    vbroadcastsd ymm2, xmm2         ; ymm2 <- low part of xmm2 * 4
+    vsubpd ymm1, ymm1, ymm2         ; ymm1 = ymm1 - ymm2
+    vmovupd [rcx], ymm1             ; A <- ymm1 (A - scalar)
     ret
 subtract_scalar_from_vector endp
 
 ;; TODO: check if this works
 mul_vector_by_scalar proc
     vmovupd ymm1, [rcx]             ; ymm1 <- array A at addr rcx
-    vmovapd xmm2, [rdx]			; xmm2 <- scalar
-    vbroadcastsd ymm2, xmm2		; ymm2 <- low part of xmm2 * 4
+    vmovapd xmm2, [rdx]             ; xmm2 <- scalar
+    vbroadcastsd ymm2, xmm2         ; ymm2 <- low part of xmm2 * 4
     vmulpd ymm1, ymm1, ymm2         ; ymm1 = ymm1 * ymm2
     vmovupd [rcx], ymm1             ; A <- ymm1 (A * scalar)
     ret
@@ -75,13 +75,17 @@ mul_vector_by_scalar endp
 
 ;; TODO: check if this works
 mul_matrix_by_scalar proc
-    vmovapd xmm2, [rdx]		; xmm2 <- scalar
-    vbroadcastsd ymm2, xmm2	; ymm2 <- low part of xmm2 * 4
+    xor eax, eax                ; eax = 0
+    mov ebx, 20h                ; ebx = 32 (32 bytes is 4 doubles = our offset)
+    vmovapd xmm2, [rdx]         ; xmm2 <- scalar
+    vbroadcastsd ymm2, xmm2     ; ymm2 <- low part of xmm2 * 4
     mov ecx, 0                  ; ecx <- 0 (counter)
 loop_label:
-    vmovupd ymm1, [rcx][ecx*8]  ; ymm1 <- array A (a row in matrix) at addr rcx + offset
+    mov eax, ecx                ; eax = ecx (loop counter)
+    mul ebx                     ; eax = ecx * 32
+    vmovupd ymm1, [rcx][eax]    ; ymm1 <- array A (a row in matrix) at addr rcx + offset
     vmulpd ymm1, ymm1, ymm2     ; ymm1 = ymm1 * ymm2
-    vmovupd [rcx][exc*8], ymm1  ; A <- ymm1 (A * scalar)
+    vmovupd [rcx][eax], ymm1    ; A <- ymm1 (A * scalar)
     inc ecx                     ; increment counter
     cmp ecx, 3                  ; check if 4 passes have been made
     jne loop_label              ; jump to loop_label if not
@@ -89,5 +93,40 @@ loop_label:
 mul_matrix_by_scalar endp
 
 
-END
-;; === KONIEC nn.asm === ;;
+    ;; === OPERATIONS WITH MATRICES ===
+;; TODO: check if this works
+add_matrices proc
+    xor eax, eax                    ; eax = 0
+    mov ebx, 20h                    ; ebx = offset
+    mov ecx, 0                      ; ecx <- 0 (counter)
+loop_label:
+    mov eax, ecx                    ; eax = ecx
+    mul ebx                         ; eax = ecx * 32
+    vmovupd ymm1, [rcx][eax]        ; ymm1 <- array A (a row in matrix) at addr rcx + offset
+    vaddpd ymm1, ymm1, [rdx][eax]   ; ymm1 = ymm1 + ymm2
+    vmovupd [rcx][eax], ymm1        ; A <- ymm1 (A + B)
+    inc ecx                         ; increment counter
+    cmp ecx, 3                      ; check if 4 passes have been made
+    jne loop_label                  ; jump to loop_label if not
+    ret
+add_matrices endp
+
+;; TODO: check if this works
+subtract_matrices proc
+    xor eax, eax                    ; eax = 0
+    mov ebx, 20h                    ; ebx = offset
+    mov ecx, 0                      ; ecx <- 0 (counter)
+loop_label:
+    mov eax, ecx                    ; eax = ecx
+    mul ebx                         ; eax = ecx * 32
+    vmovupd ymm1, [rcx][eax]        ; ymm1 <- array A (a row in matrix) at addr rcx + offset
+    vsubpd ymm1, ymm1, [rdx][eax]   ; ymm1 = ymm1 - ymm2
+    vmovupd [rcx][eax], ymm1        ; A <- ymm1 (A - B)
+    inc ecx                         ; increment counter
+    cmp ecx, 3                      ; check if 4 passes have been made
+    jne loop_label                  ; jump to loop_label if not
+    ret
+add_matrices endp
+
+
+END ;; === KONIEC nn.asm === ;;
